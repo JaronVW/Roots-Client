b2cf790b1bee57c44a0829a05d137e00c9bc32ff
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-12-14 14:45:59 +0100

    Added a bunch of inputs to the create event page, and made the buttons function.

--- src/app/events/addevent/addevent.component.ts
+++ src/app/events/addevent/addevent.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addevent',
   templateUrl: './addevent.component.html',
-  styles: ['button { width: 80%; }', '.form-check, button, h1 { margin-top: 10px; }'],
+  styles: [
+    'button { width: 100%; margin-top: 10px; }',
+    '.form-check, .form-group { padding-top: 10px; }',
+    'h1 { padding-top: 10px; }',
+    'textarea { height: 150px; }',
+    'select { width: 100%; }'
+  ],
 })
+
 export class AddeventComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  createEvent() {
+    // add logic for creating an event here...
+    this.router.navigate(['/']);
+  }
 }
