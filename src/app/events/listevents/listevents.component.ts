c984f08ad377d61d084ddf9beeb38150bce970fb
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-12-15 12:58:04 +0100

    Moved css to css file

--- src/app/events/listevents/listevents.component.ts
+++ src/app/events/listevents/listevents.component.ts
@@ -4,14 +4,7 @@ import { SearchService } from '../../search/search.service';
 @Component({
   selector: 'app-listevents',
   templateUrl: './listevents.component.html',
-  styles: [
-    '.input { width: 60%; height: 40px; padding-left: 2.5rem; border: 2px solid transparent; border-radius: 8px; background-color: #f3f3f4; transition: .3s ease; }',
-    '.input::placeholder { color: #9e9ea7; }',
-    '.input:focus, input:hover { outline: none; border-color: rgb(234 76 137 / 10%); background-color: #fff; box-shadow: 0 0 0 4px rgb(234 76 137 / 10%); }',
-    '.icon { fill: #9e9ea7; margin: 11px 0px 0px 15px; width: 1rem; height: 1rem; position: absolute; }',
-    'button { float: right; }',
-    '.card { margin-top: 10px; }',
-  ],
+  styleUrls: ['./listevents.component.css'],
 })
 export class ListeventsComponent implements OnInit {
   searchValue: string = '';
