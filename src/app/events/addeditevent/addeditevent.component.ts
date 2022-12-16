5f45e26873b4d8e7d4e8a353e2c32b6c8c2e1c55
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-12-16 14:33:35 +0100

    Added functionality to addedit page

--- src/app/events/addeditevent/addeditevent.component.ts
+++ src/app/events/addeditevent/addeditevent.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EventService } from '../event.service';
-import { Event } from '../event.interface';
+import { Event, Tag } from '../event.interface';
 import { Location } from '@angular/common';
 
 @Component({
@@ -12,31 +12,31 @@ import { Location } from '@angular/common';
     '.form-check, .form-group { padding-top: 10px; }',
     'h1 { padding-top: 10px; }',
     'textarea { height: 111px; }',
-    '.list-group-flush { border-top-left-radius: 5px; border-bottom-left-radius: 5px; height: 160px; overflow:hidden; overflow-y:scroll; }',
+    '.list-group-flush { border-top-left-radius: 5px; border-bottom-left-radius: 5px; height: 160px; overflow:hidden; overflow-y:scroll; background-color: white; }',
     'a { text-decoration: none; }',
     'a:hover { text-decoration: underline; }',
     '.validField {  border:2px solid green; display: block;  }',
     '.invalidField {  border:2px solid red; display: block;  }',
-    '::ng-deep .multiselect-dropdown .dropdown-btn {width: -webkit-fill-available !important}',
+    '::ng-deep .multiselect-dropdown .dropdown-btn {width: -webkit-fill-available !important; }',
     '::ng-deep .multiselect-dropdown .dropdown-btn:focus {outline: none !important}',
+    '.form-tags { background-color: white; border-radius: 5px; }',
   ],
 })
 export class AddediteventComponent implements OnInit {
-  title: string = '';
-  description: string = '';
-  date: Date = new Date();
-
   dropdownList: any = [];
   selectedItems: any = [];
   dropdownSettings = {};
-  requiredField: boolean = false;
+  error: boolean = false;
+  errorMessage: string = '';
+  isFirstVisit: boolean = true;
+  isEditing: boolean = false;
 
   event: Event = {
     title: '',
     description: '',
-    date: new Date(),
+    dateOfEvent: new Date(),
     // userId: 'test',
-    // tags: ['test', 'test2'],
+    tags: [{subject: ''}],
     // customtags ['test', 'test2'],
     // multiMedia: ['test', 'test2'],
   };
@@ -51,17 +51,18 @@ export class AddediteventComponent implements OnInit {
   ngOnInit() {
     var id = this.route.snapshot.paramMap.get('id');
     if (id) {
+      this.isEditing = true;
       this.EventService.getEvent(id).subscribe((response: Event) => {
         this.event = response;
+        console.log(this.event)
+        if (response.dateOfEvent) this.event.dateOfEvent = new Date(response.dateOfEvent);
       });
     }
-    
+
     this.dropdownList = [
-      { item_id: 1, item_text: 'Test1' },
-      { item_id: 2, item_text: 'Test2' },
-      { item_id: 3, item_text: 'Test3' },
-      { item_id: 4, item_text: 'Test4' },
-      { item_id: 5, item_text: 'Test5' },
+      { item_id: 1, item_text: 'Finances' },
+      { item_id: 2, item_text: 'Relocation' },
+      { item_id: 3, item_text: 'Work culture' },
     ];
     this.selectedItems = [];
 
@@ -74,27 +75,48 @@ export class AddediteventComponent implements OnInit {
       itemsShowLimit: 5,
       allowSearchFilter: true,
     };
-    this.setStatus();
   }
 
-  setStatus() {
-    this.selectedItems.length > 0
-      ? (this.requiredField = true)
-      : (this.requiredField = false);
+  createEvent() {
+    const data = {
+      title: this.event.title,
+      description: this.event.description,
+    };
+    this.EventService.addEvent(data).subscribe((response: Event) => {
+      this.router.navigate(['/events']);
+      console.log(response);
+    });
+  }
+
+  validate() {
+    if (this.selectedItems.length <= 0) this.setError(true, 'You must select at least 1 tag.');
+    if (this.event.description == '') this.setError(true, 'Description can not be empty.');
+    if (this.event.title == '') this.setError(true, 'Title can not be empty.');
+    if (this.event.title != '' && this.event.description != '' && this.selectedItems.length > 0) {
+      this.setError(false, '');
+      if (this.isEditing) {
+        console.log("Editing man")
+      } else {
+        this.createEvent();
+      }
+    } 
+  }
+
+  setError(error: boolean, errorMessage: string) {
+    this.error = error;
+    this.errorMessage = errorMessage;
   }
 
   onItemSelect(item: any) {
     //Do something if required
     console.log(item);
     this.addToSelectedItems(item);
-    this.setClass();
     console.log(this.selectedItems);
   }
   onSelectAll(items: any) {
     //Do something if required
     console.log(items);
     this.addToSelectedItems(items);
-    this.setClass();
     console.log(this.selectedItems);
   }
 
@@ -119,27 +141,6 @@ export class AddediteventComponent implements OnInit {
         }
       }
     }
-    this.setClass();
-  }
-
-  setClass() {
-    this.setStatus();
-    if (this.selectedItems.length > 0) {
-      return 'validField';
-    } else {
-      return 'invalidField';
-    }
-  }
-
-  createEvent() {
-    const data = {
-      title: this.event.title,
-      description: this.event.description,
-    };
-    this.EventService.addEvent(data).subscribe((response: Event) => {
-      this.router.navigate(['/events']);
-      console.log(response);
-    });
   }
 
   backClicked() {
--- src/app/events/event.interface.ts
+++ src/app/events/event.interface.ts
@@ -1,12 +1,16 @@
 export interface Event {
   title: string;
   description: string;
-  date?: Date;
-  tags?: string[];
+  dateOfEvent?: Date;
+  tags?: Tag[];
   files?: string[];
   userId?: string;
 
   multiMedia?: string[];
 
   //   Nog te doen: Tags, files, userId referencen naar andere interfaces
+}
+
+export interface Tag {
+  subject: string;
 }
