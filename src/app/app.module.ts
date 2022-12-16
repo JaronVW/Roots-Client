3fbd2519f0aa0a1de01cc426e2dbb04426065087
Matthijs Feringa <mt.feringa@student.avans.nl>
2022-12-16 12:25:17 +0100

    added a multiselector for tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component'
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import {RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
-import {routes} from "./app.routing";
+import { routes } from './app.routing';
 import { AddeventComponent } from './events/addevent/addevent.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './search/search.component';
-import {FormsModule} from "@angular/forms";
-import {SearchService} from "./search/search.service";
+import { FormsModule } from '@angular/forms';
+import { SearchService } from './search/search.service';
 import { ListeventsComponent } from './events/listevents/listevents.component';
-import {TokenInterceptor} from "../../token.interceptor";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { TokenInterceptor } from '../../token.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -36,14 +37,12 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
     NgbModule,
     FormsModule,
     HttpClientModule,
-
+    NgMultiSelectDropDownModule.forRoot(),
   ],
   providers: [
     SearchService,
-    {provide: HTTP_INTERCEPTORS , useClass: TokenInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-
+export class AppModule {}
