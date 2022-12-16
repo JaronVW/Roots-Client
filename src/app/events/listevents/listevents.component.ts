32d282567b5c875bb9a7d4923008ae9c311edaa7
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-12-16 11:07:16 +0100

    Inserted EventService into listevents.component

--- src/app/events/listevents/listevents.component.ts
+++ src/app/events/listevents/listevents.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../search/search.service';
+import { EventService } from '../event.service';
+import { Event } from '../event.interface';
 
 @Component({
   selector: 'app-listevents',
@@ -8,11 +10,16 @@ import { SearchService } from '../../search/search.service';
 })
 export class ListeventsComponent implements OnInit {
   searchValue: string = '';
+  events: Array<Event> = new Array<Event>;
 
-  constructor(private SearchService: SearchService, 
-    ) {}
+  constructor(private SearchService: SearchService, private EventService: EventService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.EventService.getEvents().subscribe((response) => {
+      this.events = response;
+    });
+    console.log(this.events);
+  }
 
   search(value: any) {
     console.log('reached component', value);
