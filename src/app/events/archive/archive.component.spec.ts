45ba5572a88e8b0433a7d6b543394d20c877fd89
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-01-06 16:08:33 +0100

    made some more tests for listevents and working on tests for archived items

--- src/app/events/archive/archive.component.spec.ts
+++ src/app/events/archive/archive.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { mockArchiveEvent1, mockArchivedEvents } from './archive.mockdata';
 
 import { ArchiveComponent } from './archive.component';
+import { By } from '@angular/platform-browser';
 
 describe('ArchiveComponent', () => {
   let component: ArchiveComponent;
@@ -21,4 +23,16 @@ describe('ArchiveComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('there should be one archived event', fakeAsync(() => {
+    component.events = mockArchivedEvents;
+    console.log(component.events);
+    fixture.detectChanges();
+    console.log(fixture.debugElement.nativeElement);
+    const compiled = fixture.debugElement.queryAll(By.css('.archived-event-row'));
+    console.log(compiled);
+    // console.log(compiled.querySelector('.archived-events-row'));
+    // console.log(compiled.querySelector('.archived-event'));
+    // expect(compiled.querySelector('.archived-events-row')).toContain(compiled.querySelector('.archived-event'));
+  }));
 });
