d392981444515af16bf3b86ad1289aa4c1d9864c
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-01-08 00:05:49 +0100

    added some simple tests for checking if all fields are present

--- src/app/events/addeditevent/addeditevent.component.spec.ts
+++ src/app/events/addeditevent/addeditevent.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
@@ -40,5 +41,31 @@ describe('AddeventComponent', () => {
     button.click();
     tick();
     expect(component.validate).toHaveBeenCalled();
+  }));
+
+  //testing if all fields are present
+  it('there should be a field for the event title', fakeAsync(() => {
+    let compiled = fixture.debugElement.queryAll(By.css('.title'));
+    expect(compiled.length).toEqual(1);
+  }));
+
+  it('there should be a field for the event description', fakeAsync(() => {
+    let compiled = fixture.debugElement.queryAll(By.css('#description'));
+    expect(compiled.length).toEqual(1);
+  }));
+
+  it('there should be a field for the event date', fakeAsync(() => {
+    let compiled = fixture.debugElement.queryAll(By.css('#dateOfEvent'));
+    expect(compiled.length).toEqual(1);
+  }));
+
+  it('there should be a field for the event tags', fakeAsync(() => {
+    let compiled = fixture.debugElement.queryAll(By.css('#tags'));
+    expect(compiled.length).toEqual(1);
+  }));
+
+  it('there should be a field for the event files', fakeAsync(() => {
+    let compiled = fixture.debugElement.queryAll(By.css('#file'));
+    expect(compiled.length).toEqual(1);
   }));
 });
