753cd8c78ac24d2d6dcfebc166956a5789b9d4bf
TimothyBorghouts <timothy.borghouts2@gmail.com>
2023-01-11 12:11:02 +0100

    Created layout for organizations accordion and added text feedback of domain when entering email.

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public isCollapsed = false;
+  email: String = '';
+  domain: String = '';
   constructor() {
     /* TODO document why this constructor is empty */
   }
 
   ngOnInit(): void {
     // TODO document why this method 'ngOnInit' is empty
+  }
+
+  createDomain() {
+    this.domain = this.email.split('@')[1];
   }
 }
