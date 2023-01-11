4c285d442d1e82c311a1c5ab9a9b289628f8505a
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2023-01-11 13:20:14 +0100

    Added validation with testdata.

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public isCollapsed = false;
+  organization = {
+    name: '',
+    domain: '',
+  };
   email: String = '';
-  domain: String = '';
+  error: boolean = false;
+  errorMessage: String = '';
+  testOrganizationName: String = 'iHomer';
+  testDomain: String = 'gmail.com';
+
   constructor() {
     /* TODO document why this constructor is empty */
   }
@@ -18,6 +26,38 @@ export class RegisterComponent implements OnInit {
   }
 
   createDomain() {
-    this.domain = this.email.split('@')[1];
+    this.organization.domain = this.email.split('@')[1];
+  }
+
+  validate() {
+    if (this.testOrganizationName == this.organization.name) {
+      this.setError(true, 'Deze organisatie bestaat al.');
+      throw new Error('Deze organisatie bestaat al.');
+    }
+    if (this.organization.name == '') {
+      this.setError(true, 'Organisatienaam kan niet leeg zijn.');
+      throw new Error('Organisatienaam kan niet leeg zijn.');
+    }
+    if (this.testDomain == this.organization.domain) {
+      this.setError(true, 'Dit domein bestaat al.');
+      throw new Error('Dit domein bestaat al.');
+    }
+    if (this.organization.domain == '') {
+      this.setError(true, 'Domein kan niet leeg zijn; voer uw email in.');
+      throw new Error('Domein kan niet leeg zijn; voer uw email in.');
+    }
+    if (
+      this.organization.name != '' &&
+      this.testOrganizationName != this.organization.name &&
+      this.organization.domain != '' &&
+      this.testDomain != this.organization.domain 
+    ) {
+      this.setError(false, '');
+    }
+  }
+
+  setError(error: boolean, errorMessage: string) {
+    this.error = error;
+    this.errorMessage = errorMessage;
   }
 }
