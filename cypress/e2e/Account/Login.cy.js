7bf9747319b9c217d8cadc23ed9ad8030217e7b9
Thomas <vanotterloo.thomas@gmail.com>
2023-01-13 12:52:30 +0100

    test for login

--- cypress/e2e/Account/Login.cy.js
+++ cypress/e2e/Account/Login.cy.js
@@ -4,11 +4,9 @@ describe('Testing the register page functionalities.', () => {
 
   })
 
-
-
   it('Search for a text input field for email and password and writes in it.', () => {
     cy.get('#emailInput').type('JohnDoe@Chicken.com')
-    cy.get('#passwordInput').type('123456')
+    cy.get('#passwordInput').type('nQK46(+C\\C\'Q')
   })
 
   it('The hyperlink that shows "Registreren" redirects the user to the register page', () => {
@@ -21,6 +19,37 @@ describe('Testing the register page functionalities.', () => {
     cy.get('a').contains('hier').click()
     cy.wait(100)
     cy.url().should('include', 'http://localhost:4200/accountrecovery')
+  })
+
+
+
+  it('writes an fake email and password that does not exist. Should return an error.', () => {
+    cy.get('#emailInput').type('CompletleyFake@iSwear.com')
+    cy.get('#passwordInput').type('IfYouReadThisYouAreAwesome!')
+    cy.get('.btn').contains('Inloggen').click().wait(100)
+    cy.contains('Email of wachtwoord incorrect.').should('have.length', 1).should('be.visible')
+
+  })
+
+
+  it('writes an invalid email. Should return an error.', () => {
+    cy.get('#emailInput').type('CompletleyFake@.com')
+    cy.get('#passwordInput').type('IfYouReadThisYouAreAwesome!')
+    cy.get('.btn').contains('Inloggen').click().wait(100)
+    cy.contains('Email moet valide zijn.').should('have.length', 1).should('be.visible')
+
+    cy.get('#emailInput').clear().type('CompletleyFake@')
+    cy.get('.btn').contains('Inloggen').click().wait(100)
+    cy.contains('Email moet valide zijn.').should('have.length', 1).should('be.visible')
+
+
+    cy.get('#emailInput').clear().type('CompletleyFake.com')
+    cy.get('.btn').contains('Inloggen').click().wait(100)
+    cy.contains('Email moet valide zijn.').should('have.length', 1).should('be.visible')
+
+    cy.get('#emailInput').clear().type('CompletleyFake')
+    cy.get('.btn').contains('Inloggen').click().wait(100)
+    cy.contains('Email moet valide zijn.').should('have.length', 1).should('be.visible')
   })
 
 
