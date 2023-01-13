32da68e7878e9eeaf258664f86b03d8c58e29e82
Matthijs Feringa <mt.feringa@student.avans.nl>
2023-01-13 14:35:16 +0100

    made some tests for sprint 3

--- src/app/authentication/register/register.component.spec.ts
+++ src/app/authentication/register/register.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RegisterComponent } from './register.component';
 
@@ -9,6 +11,7 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      imports: [NgbCollapseModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
@@ -18,5 +21,35 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a form', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form')).toBeTruthy();
+  });
+
+  it('should have a firstName input', () => {
+    let compiled = fixture.debugElement.queryAll(By.css('#firstNameInput'));
+    expect(compiled.length).toEqual(1);
+  });
+
+  it('should have a lastName input', () => {
+    let compiled = fixture.debugElement.queryAll(By.css('#lastNameInput'));
+    expect(compiled.length).toEqual(1);
+  });
+
+  it('should have a email input', () => {
+    let compiled = fixture.debugElement.queryAll(By.css('#emailInput'));
+    expect(compiled.length).toEqual(1);
+  });
+
+  it('should have a password input', () => {
+    let compiled = fixture.debugElement.queryAll(By.css('#passwordInput'));
+    expect(compiled.length).toEqual(1);
+  });
+
+  it('should have a confirmPassword input', () => {
+    let compiled = fixture.debugElement.queryAll(By.css('#passwordInputConfirm'));
+    expect(compiled.length).toEqual(1);
   });
 });
