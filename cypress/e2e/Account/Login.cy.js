0554b52a8578076ae60b9ed575a9d64064bdbe1c
Thomas <vanotterloo.thomas@gmail.com>
2023-01-16 09:45:56 +0100

    login test

--- cypress/e2e/Account/Login.cy.js
+++ cypress/e2e/Account/Login.cy.js
@@ -4,6 +4,36 @@ describe('Testing the register page functionalities.', () => {
 
   })
 
+
+  it('Enters valid account information. Should log in, and redirect to the next page', () => {
+    cy.intercept('POST', 'http://localhost:3000/auth/login', ( res) => {
+      res.reply({
+        statusCode: 201,
+        body: {
+          "access_token":"fakeTokenYayyy3123assadas.eyJ1c2VybmFtZSI6InRob21hc0Bqam10dGwubmwiLCJzdWIiOjYsIm9yZ2FuaXNhdGlvbklkIjpudWxsLCJpYXQiOjE2NzM2Mjk3MDksImV4cCI6MTY3MzcxNjEwOX0.ahS-CBkO2jRJlba1811SL_m9TChm9E9zQKs4Bb1CbEg",
+        }
+      })
+    }).as('login')
+
+
+
+    cy.get('#emailInput').type('CompletleyFake@iSwear.com')
+    cy.get('#passwordInput').type('IfYouReadThisYouAreAwesome!')
+    cy.get('.btn').contains('Inloggen').click()
+    cy.wait('@login')
+
+    cy.get('@login').then((interception) => {
+      expect(interception.response.statusCode).to.equal(201)
+    }).wait(300)
+
+    // cy.url().should('include', 'http://localhost:4200/events')
+
+    cy.window()
+      .then(win => {
+        expect(win.localStorage.getItem('token')).to.exist
+      })
+  })
+
   it('Search for a text input field for email and password and writes in it.', () => {
     cy.get('#emailInput').type('JohnDoe@Chicken.com')
     cy.get('#passwordInput').type('nQK46(+C\\C\'Q')
@@ -51,6 +81,8 @@ describe('Testing the register page functionalities.', () => {
     cy.get('.btn').contains('Inloggen').click().wait(100)
     cy.contains('Email moet valide zijn.').should('have.length', 1).should('be.visible')
   })
+
+
 
 
 })
